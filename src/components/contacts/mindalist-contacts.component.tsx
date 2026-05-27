2bc64c6ebcd74051361ef72747c8e314aa351b04
mindalist <manfro.andrea@gmail.com>
2026-05-27 23:23:55 +0200

    fix sulla mail

--- src/components/contacts/mindalist-contacts.component.tsx
+++ src/components/contacts/mindalist-contacts.component.tsx
@@ -66,10 +66,10 @@ export default function MindalistContacts() {
                 />
               </svg>
               <a
-                href="mailto:manfro.andrea@gmail.com"
+                href="mailto:info@mindalist.it"
                 className="min-w-0 break-all"
               >
-                manfro.andrea@gmail.com
+                info@mindalist.it
               </a>
             </div>
             <div className="flex items-center mt-2 gap-2 text-dark-600 dark:text-gray-400 hover:text-white">
@@ -88,7 +88,6 @@ export default function MindalistContacts() {
                   d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
                 />
               </svg>
-              <a href="tel:3519977028">+39 351 997 7028</a>
             </div>
           </div>
         </div>
